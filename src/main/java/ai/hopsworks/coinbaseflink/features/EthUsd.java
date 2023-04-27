aad5815ced15b89db664533ca28fc7d8638777c2
Fabio Buso <fabio@logicalclocks.com>
2023-04-28 01:40:57 +0200

    Cleanup

--- src/main/java/ai/hopsworks/coinbaseflink/features/EthUsd.java
+++ src/main/java/ai/hopsworks/coinbaseflink/features/EthUsd.java
@@ -20,45 +20,26 @@ public class EthUsd {
   private StreamFeatureGroup streamFeatureGroup;
 
   public EthUsd() throws Exception {
-    // FIXME: why can't I use the builder?
-    HopsworksConnection hopsworksConnection = new HopsworksConnection(
-        "ef3beed0-e437-11ed-9473-fd53c07a424c.cloud.hopsworks.ai",
-        443,
-        "mischievous",
-        null,
-        null,
-        false,
-        null,
-        null,
-        null,
-        "wJP4J8zVO76abcZs.6SETE5tTgqGsTogRLxKPtfLLU37pn3G5zcqj0HQjDDMrwDfOqe6OK0BQg37aMolV"
-    );
+    HopsworksConnection hopsworksConnection = HopsworksConnection.builder()
+        .host("ef3beed0-e437-11ed-9473-fd53c07a424c.cloud.hopsworks.ai")
+        .project("mischievous")
+        .apiKeyValue("wJP4J8zVO76abcZs.6SETE5tTgqGsTogRLxKPtfLLU37pn3G5zcqj0HQjDDMrwDfOqe6OK0BQg37aMolV")
+        .build();
+
     FeatureStore featureStore = hopsworksConnection.getFeatureStore();
     streamFeatureGroup = featureStore.getStreamFeatureGroup("ticker", 2);
   }
 
   public void stream() throws Exception {
     StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-    env.getConfig().enableObjectReuse();
-    Class<?> unmodColl = Class.forName("java.util.Collections$UnmodifiableCollection");
-    env.getConfig().addDefaultKryoSerializer(unmodColl, UnmodifiableCollectionSerializer.class);
-
-
     env.setParallelism(1);
 
     DataStreamSource<Ticker> websocketStream = env.addSource(new WSReader());
 
-    //StreamingFileSink<String> streamingFileSink = StreamingFileSink
-    //    .forRowFormat(new Path("hdfs:///Projects/flinke/data/test"),
-    //        new SimpleStringEncoder<String>("UTF-8"))
-    //     .build();
+    streamFeatureGroup.insertStream(websocketStream);
 
-    //websocketStream.map(Ticker::toString).addSink(streamingFileSink);
-
-     streamFeatureGroup.insertStream(websocketStream);
-
-     env.execute(JOB_NAME);
-     env.enableCheckpointing(CHECKPOINTING_INTERVAL_MS);
-     env.setRestartStrategy(RestartStrategies.noRestart());
+    env.execute(JOB_NAME);
+    env.enableCheckpointing(CHECKPOINTING_INTERVAL_MS);
+    env.setRestartStrategy(RestartStrategies.noRestart());
   }
 }
