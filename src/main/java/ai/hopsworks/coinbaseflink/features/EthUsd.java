0784d6daef2635d367229c3914cd710f2cf4f6e4
Fabio Buso <fabio@hopsworks.ai>
2023-06-21 10:21:06 +0200

    Add price window aggregation

--- src/main/java/ai/hopsworks/coinbaseflink/features/EthUsd.java
+++ src/main/java/ai/hopsworks/coinbaseflink/features/EthUsd.java
@@ -1,5 +1,6 @@
 package ai.hopsworks.coinbaseflink.features;
 
+import ai.hopsworks.coinbaseflink.utils.Ticker;
 import ai.hopsworks.coinbaseflink.utils.WSReader;
 import com.logicalclocks.hsfs.flink.FeatureStore;
 import com.logicalclocks.hsfs.flink.HopsworksConnection;
@@ -8,9 +9,12 @@ import com.twitter.chill.java.UnmodifiableCollectionSerializer;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
 
 public class EthUsd {
 
@@ -21,20 +25,23 @@ public class EthUsd {
 
   public EthUsd() throws Exception {
     HopsworksConnection hopsworksConnection = HopsworksConnection.builder()
-        .host("ef3beed0-e437-11ed-9473-fd53c07a424c.cloud.hopsworks.ai")
-        .project("mischievous")
-        .apiKeyValue("wJP4J8zVO76abcZs.6SETE5tTgqGsTogRLxKPtfLLU37pn3G5zcqj0HQjDDMrwDfOqe6OK0BQg37aMolV")
+        .host("41074450-096b-11ee-96ca-a76595e0c7d6.cloud.hopsworks.ai")
+        .project("eth_flink")
+        .apiKeyValue("N7N7p0c5cB1zlC11.WylW9I6ZOUS77tcgSw1iSUwTHTJAaa5kWl0yhY5HqmUEuvwqECd7APM4ksbPanjP")
         .build();
 
     FeatureStore featureStore = hopsworksConnection.getFeatureStore();
-    streamFeatureGroup = featureStore.getStreamFeatureGroup("ticker", 2);
+    streamFeatureGroup = featureStore.getStreamFeatureGroup("eth_usd_price_5_min", 1);
   }
 
   public void stream() throws Exception {
     StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
     env.setParallelism(1);
 
-    DataStreamSource<Ticker> websocketStream = env.addSource(new WSReader());
+    DataStream<Price5Minutes> websocketStream = env.addSource(new WSReader())
+        .keyBy(Ticker::getTicker)
+        .window(SlidingEventTimeWindows.of(Time.minutes(5), Time.minutes(1)))
+        .aggregate(new PriceAccumulators());
 
     streamFeatureGroup.insertStream(websocketStream);
 
---
+++ src/main/java/ai/hopsworks/coinbaseflink/features/PriceAccumulators.java
@@ -0,0 +1,58 @@
+package ai.hopsworks.coinbaseflink.features;
+
+import ai.hopsworks.coinbaseflink.utils.Ticker;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.tuple.Tuple5;
+
+import java.time.Instant;
+
+public class PriceAccumulators implements AggregateFunction<Ticker, Tuple5<String, Long, Float, Float, Float>, Price5Minutes> {
+
+  /*
+   * Tuple composition is the following:
+   * - String: Ticker
+   * - Number of events processed
+   * - Price accumulator
+   * - Max Price
+   * - Min price
+   */
+
+  @Override
+  public Tuple5<String, Long, Float, Float, Float> createAccumulator() {
+    return new Tuple5<>("", 0L, 0.0f, Float.MIN_VALUE, Float.MAX_VALUE);
+  }
+
+  @Override
+  public Tuple5<String, Long, Float, Float, Float> add(Ticker ticker,
+                                                       Tuple5<String, Long, Float, Float, Float> tuple) {
+    return new Tuple5<>(
+        ticker.getTicker(),
+        tuple.f1 + 1,
+        tuple.f2 + ticker.getPrice(),
+        tuple.f3 > ticker.getPrice() ? tuple.f3 : ticker.getPrice(),
+        tuple.f4 < ticker.getPrice() ? tuple.f4 : ticker.getPrice());
+  }
+
+  @Override
+  public Price5Minutes getResult(Tuple5<String, Long, Float, Float, Float> tuple) {
+    return new Price5Minutes(
+        tuple.f0,
+        Instant.now(),
+        tuple.f2 / tuple.f1,
+        tuple.f3,
+        tuple.f4
+    );
+  }
+
+  @Override
+  public Tuple5<String, Long, Float, Float, Float> merge(Tuple5<String, Long, Float, Float, Float> tuple1,
+                                                         Tuple5<String, Long, Float, Float, Float> tuple2) {
+    return new Tuple5<>(
+        tuple1.f0,
+        tuple1.f1 + tuple2.f1,
+        tuple1.f2 + tuple2.f2,
+        tuple1.f3 > tuple2.f3 ? tuple1.f3 : tuple2.f3,
+        tuple1.f4 < tuple2.f4 ? tuple1.f4 : tuple2.f4
+    );
+  }
+}
